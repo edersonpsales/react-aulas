a0bf001acf1651d28b6af1825625fb11a8cb4b61
edersonpsales <ederson.psales@gmail.com>
2024-06-18 11:17:03 -0300

    Apos a aula 10

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,33 +1,32 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const inputPassword = useRef<HTMLInputElement>(null);
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleEntrar = useCallback(() =>{
+  const handleEntrar = useCallback(() => {
     console.log(email);
     console.log(password);
-  },[email,password]);
 
-  
-  useEffect(()=>{
-    alert('componente inicializou');
-  },[]);
-  
-  useEffect(()=>{
+    if (inputPassword.current !== null) {
+      inputPassword.current.focus();
+    }
+  }, [email, password]);
+
+  useEffect(() => {
+    alert("componente inicializou");
+  }, []);
+
+  useEffect(() => {
     console.log(email);
-  },[email]);
+  }, [email]);
 
-
-  const mailLength = useMemo(()=>{
+  const mailLength = useMemo(() => {
     return Math.random();
-  },[])
+  }, []);
 
   //const mailLength =  Math.random();
-
-
-  
 
   return (
     <div>
@@ -35,13 +34,26 @@ export const Login = () => {
         <p>qtd de caracterres : {mailLength}</p>
         <label>
           <span>e-mail</span>
-          <input value={email} onChange={ e => setEmail(e.target.value)}/>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            onKeyDown={(e) =>
+              e.key === "Enter" ? inputPassword.current?.focus() : undefined
+            }
+          />
         </label>
         <label>
           <span>Senha</span>
-          <input type="password" value={password} onChange={ e => setPassword(e.target.value)}/>
+          <input
+            ref={inputPassword}
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
-        <button type="button" onClick={handleEntrar}>Entrar</button>
+        <button type="button" onClick={handleEntrar}>
+          Entrar
+        </button>
       </form>
       <span>{email}</span>
     </div>
